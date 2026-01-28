406522974af2288745805b1156b8726f152872ec
JaderoChan <c_dl_cn@outlook.com>
2026-01-28 20:51:44 +0800

    update

--- src/language.cpp
+++ src/language.cpp
@@ -1,6 +1,7 @@
 #include "language.h"
 
 #include <qapplication.h>
+#include <qdir.h>
 #include <qevent.h>
 
 #include <easy_translate.hpp>
@@ -8,16 +9,13 @@
 
 #include "config.h"
 
-void initLanguage()
-{
-    easytr::Languages langs;
-    langs.add("EN", (QApplication::applicationDirPath() + "/languages/en.json").toStdString());
-    langs.add("ZH", (QApplication::applicationDirPath() + "/languages/zh.json").toStdString());
-    easytr::setLanguages(langs);
-}
-
 QString setLanguage(const QString& langId)
 {
+    QDir::setCurrent(QApplication::applicationDirPath());
+    easytr::setLanguages(APP_LANG_FILENAME);
+    if (easytr::languages().empty())
+        mlog::info("Invalid or empty Languages file");
+
     std::string id = langId.toStdString();
     if (easytr::hasLanguage(id))
     {
@@ -30,11 +28,19 @@ QString setLanguage(const QString& langId)
     else
     {
         mlog::info("Expected language is missing, try fall back to the default language");
-        id = easytr::languages().getIds().front();
-        if (easytr::setCurrentLanguage(id))
-            mlog::info("Successfully fall back to language: {}", id.c_str());
+        if (easytr::languages().empty())
+        {
+            mlog::warning("Not find any language");
+            return easytr::currentLanguage();
+        }
         else
-            mlog::warning("Failed to fall back to language: {}", id.c_str());
+        {
+            id = easytr::languages().getIds().front();
+            if (easytr::setCurrentLanguage(id))
+                mlog::info("Successfully fall back to language: {}", id.c_str());
+            else
+                mlog::warning("Failed to fall back to language: {}", id.c_str());
+        }
     }
 
     QEvent event(QEvent::Type::LanguageChange);
--- src/language.h
+++ src/language.h
@@ -2,6 +2,4 @@
 
 #include <qstring.h>
 
-void initLanguage();
-
 QString setLanguage(const QString& langId);
