c3405d83ea16bed642c0850b0608451d682c5235
JaderoChan <c_dl_cn@outlook.com>
2026-02-24 15:58:46 +0800

    remove 3rdparty

--- cmake/FetchGlobalHotkey.cmake
+++
@@ -1,7 +0,0 @@
-include(FetchContent)
-FetchContent_Declare(
-    global_hotkey
-    GIT_REPOSITORY https://github.com/JaderoChan/global_hotkey.git
-    GIT_TAG v2.1.0
-)
-FetchContent_MakeAvailable(global_hotkey)
